733e5f9d6f55c6380a63ab5641b5cf56e4dd76c0
puffer <zaranlan@gmail.com>
2025-09-30 19:29:41 -0600

    Stylization of the navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className='container'>
-        <Navbar />
+      <>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route path='/' exact />
+          </Routes>
+        </Router>
+      </>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Button} from './Button';
 import './Navbar.css'
+import {Link} from 'react-router-dom'
 import logo from '../assets/logo.png' 
 
-const Navbar = () => {
-  return (
-    <div className='navbar-navigator'>
-        <img src={logo} alt="" className='logo' />
-        <ul className='nav-menu'>
-            <li className='nav-item'>Home</li>
-            <li className='nav-item'>About</li>
-            <li className='nav-item'>Rules</li>
-            <li className='nav-item'>Vote</li>
-            <li className='nav-item'>Staff</li>
-            <li className='nav-item'>Store</li>
-        </ul>
-    </div>
-  )
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        setButton(window.innerWidth <= 960)
+    }
+
+    window.addEventListener('resize', showButton)
+
+    return (
+        <div className='navbar-navigator'>
+            <Link to="/" className="navbar-logo">
+                <img src={logo} alt="" className='logo' /> AuroraCraft
+            </Link>
+
+
+            <ul className='nav-menu'>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    Home
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                    About
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/rules' className='nav-links' onClick={closeMobileMenu}>
+                    Rules
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/vote' className='nav-links' onClick={closeMobileMenu}>
+                    Vote
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/staff' className='nav-links' onClick={closeMobileMenu}>
+                    Staff
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/store' className='nav-links' onClick={closeMobileMenu}>
+                    Store
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
